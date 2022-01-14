e29b4b4e9ce23a044cc4cb4a4aa0d156f6cf8ee8
Ace <acekisch@gmail.com>
2022-01-13 20:12:10 -0800

    cleanup script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function calculateTip() {
-    let bill = document.getElementById("bill").value;
+    let bill = document.getElementById('bill').value;
     let people = document.getElementById('people').value;
 
     // null warning
@@ -7,9 +7,9 @@ function calculateTip() {
     //     document.getElementById('item-bill').getElementsByTagName("p").after("hello");
     // }
 
-    // set percentage variable to custom-input value, or else set to selected radio button
+    // set percentage variable to custom-input value if checked, else set to selected radio button
     if (document.getElementById('custom-number').checked == true ) {
-        var percentage = document.getElementById("custom-input").value;
+        var percentage = document.getElementById('custom-input').value;
     }
     else {
         var percentage = document.querySelector('input[type=radio][name=tip-percent]:checked').value;
@@ -20,40 +20,39 @@ function calculateTip() {
     let total = (bill / people) + tip ;
     
     // USD format
-    let numUSD = new Intl.NumberFormat("en-US", {
-        style:"currency",
-        currency:"USD"
+    let numUSD = new Intl.NumberFormat('en-US', {
+        style:'currency',
+        currency:'USD'
     })
     
     // update tip and total text elements
-    document.getElementById("tip").innerHTML = numUSD.format(tip);
-    document.getElementById("total").innerHTML = numUSD.format(total);
+    document.getElementById('tip').innerHTML = numUSD.format(tip);
+    document.getElementById('total').innerHTML = numUSD.format(total);
 
     //add reset button styles
-    document.getElementById("reset").classList.remove('reset-faded');
+    document.getElementById('reset').classList.remove('reset-faded');
 }
 
-
-
 // calculateTip() on key up and input click
-document.addEventListener("keyup", calculateTip);
-let ele = document.querySelectorAll("input");
+
+let ele = document.querySelectorAll('input');
     for(var i = 0; i < ele.length; i++) {
-        ele[i].addEventListener("click", calculateTip); 
+        ele[i].addEventListener('click', calculateTip); 
+        ele[i].addEventListener('keyup', calculateTip); 
     }
 
 
 //  reset
 document.getElementById("reset").onclick = function() {
     
-    document.getElementById("reset").classList.add('reset-faded');
-    document.getElementById("bill").value = "";
-    document.getElementById("people").value = "";
-    document.getElementById("tip").innerText = "$0.00";
-    document.getElementById("total").innerText = "$0.00";
+    document.getElementById('reset').classList.add('reset-faded');
+    document.getElementById('bill').value = '';
+    document.getElementById('people').value = '';
+    document.getElementById('tip').innerText = '$0.00';
+    document.getElementById('total').innerText = '$0.00';
 
     // uncheck radio
-    var btn = document.getElementsByName("tip-percent");
+    var btn = document.getElementsByName('tip-percent');
     for(var i=0;i<btn.length;i++) 
         btn[i].checked = false; 
 
@@ -62,38 +61,13 @@ document.getElementById("reset").onclick = function() {
 // nested input[text] inside input[checkbox] registers click
 document.getElementById('custom-input').onclick = function () {
     document.getElementById('custom-number').checked = true;
-    console.log("hello")
 }
 
 // reset 'custom' placeholder text on click other radio
-let radios = document.querySelectorAll("input[type=radio][name=tip-percent]:not(#custom-number)");
+let radios = document.querySelectorAll('input[type=radio][name=tip-percent]:not(#custom-number)');
 for(var i = 0; i < radios.length; i++) {
     radios[i].addEventListener('click', function() {
         document.getElementById('custom-input').value = ""
     });
 }
 
-
-
-
-
-// deprecated
-
-// // toggle display visibility for custom-number-div 
-// document.getElementById("custom-number").onclick = function() {
-//     let x = document.getElementById('custom-number-div'); 
-//     if (x.style.display === "none") {
-//      x.style.display = "block";
-//    } else {
-//      x.style.display = "none";
-//    }
-// }  
-
-
-// // hide custom-number-div if click any radio button except #custom-number
-// let radios = document.querySelectorAll("input[type=radio][name=tip-percent]:not(#custom-number)");
-// for(var i = 0; i < radios.length; i++) {
-//     radios[i].addEventListener("click", function() {
-//         document.getElementById('custom-number-div').style.display = "none";
-//     });
-// }
